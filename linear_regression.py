8ad37dd505601a19e923820274b3decdd644bcb8
mt_9 <hyunsichang@hanyang.ac.kr>
2021-05-27 13:55:33 +0900

    logistic regression

--- linear_regression.py
+++ linear_regression.py
@@ -9,8 +9,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+
 boston = load_boston()
 bostonDF = pd.DataFrame(boston.data, columns=boston.feature_names)
+bostonDF['PRICE'] = boston.target
 
 y_target = bostonDF['PRICE']
 X_data = bostonDF.drop(['PRICE'], axis=1, inplace=False)
@@ -20,11 +24,15 @@ X_train, X_test, y_train, y_test = train_test_split(X_data, y_target, test_size=
 lr = LinearRegression()
 lr.fit(X_train, y_train)
 y_preds = lr.predict(X_test)
-mse = mean_squared_error(y_test, y_preds)
-rmse = np.sqrt(mse)
-
-print("MSE : {0:.3f}, RMSE : {1:.3f}".format(mse, rmse))
-print("Variance Score: {0:.3f}".format(r2_score(y_test, y_preds)))
 
 coeff = pd.Series(data=np.round(lr.coef_, 1), index=X_data.columns)
 coeff.sort_values(ascending=False)
+
+alphas = [0,0.1,1,10,100]
+for i in alphas:
+    ridge = Ridge(alpha=i)
+    neg_mse_scores = cross_val_score(ridge, X_data, y_target, scoring="neg_mean_squared_error", cv=5)
+    rmse_scores = np.sqrt(-1 * neg_mse_scores)
+    avg_rmse = np.mean(rmse_scores)
+
+    print(avg_rmse)
