da9a02e3e3a553cd828ca24aac429f185cc3ab10
mt_9 <hyunsichang@hanyang.ac.kr>
2021-06-01 13:35:02 +0900

    example2 data preprocess

---
+++ example2.py
@@ -0,0 +1,11 @@
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data/X_test.csv", encoding='euc-kr')
+label = LabelEncoder()
+minmax = MinMaxScaler()
+df['주구매상품'] = label.fit_transform(df['주구매상품'])
+df['주구매지점'] = label.fit_transform(df['주구매지점'])
+df[:] = minmax.fit_transform(df)
+df = df.fillna(0)
