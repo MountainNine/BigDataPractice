a1b08e445f750a94c2a78d5e6b8f293ef25c2df1
mt_9 <hyunsichang@hanyang.ac.kr>
2021-06-02 15:06:08 +0900

    add pca, example2 first try

--- example2.py
+++ example2.py
@@ -1,14 +1,38 @@
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import pandas as pd
+from xgboost import XGBClassifier
 import numpy as np
 
-df = pd.read_csv("data/X_test.csv", encoding='euc-kr')
-label = LabelEncoder()
-minmax = MinMaxScaler()
-df['주구매상품'] = label.fit_transform(df['주구매상품'])
-df['주구매지점'] = label.fit_transform(df['주구매지점'])
-df[:] = minmax.fit_transform(df)
-df = df.fillna(0)
-print(df.head())
+
+def pre_process(df):
+    label = LabelEncoder()
+    scaler = StandardScaler()
+    df['주구매상품'] = label.fit_transform(df['주구매상품'])
+    df['주구매지점'] = label.fit_transform(df['주구매지점'])
+    df[:] = scaler.fit_transform(df)
+    df = df.fillna(0)
+    df =df.drop('cust_id', axis=1)
+    return df
+
+
+x = pd.read_csv("data/X_train.csv", encoding='euc-kr')
+y = pd.read_csv("data/y_train.csv")
+# x_test = pd.read_csv('data/X_test.csv', encoding='euc_kr')
+
+x = pre_process(x)
+y = y.drop('cust_id', axis=1)
+# x_train = x[:3000]
+# x_test = x[3000:]
+# y_train = y[:3000]
+# y_test = y[3000:]
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state=134)
+
+classifier = XGBClassifier()
+classifier.fit(x_train, y_train)
+pred = classifier.predict(x_test)
+acc = accuracy_score(y_test, pred)
+print(acc)
