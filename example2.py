17b5a13049d072a6f0291a63d60dd354c86218e8
mt_9 <hyunsichang@hanyang.ac.kr>
2021-06-02 13:09:28 +0900

    xgboost

--- example2.py
+++ example2.py
@@ -1,5 +1,6 @@
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 
@@ -10,3 +11,4 @@ df['주구매상품'] = label.fit_transform(df['주구매상품'])
 df['주구매지점'] = label.fit_transform(df['주구매지점'])
 df[:] = minmax.fit_transform(df)
 df = df.fillna(0)
+print(df.head())
