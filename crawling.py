47c7e90fa5423d9874f15225cb85248fc44c4028
mt_9 <hyunsichang@hanyang.ac.kr>
2021-06-08 21:48:29 +0900

    commit

--- crawling.py
+++ crawling.py
@@ -10,9 +10,9 @@ meta = soup.select('meta')
 lstRanking = soup.select(".list_ranking")[0]
 title = [x.text.strip() for x in lstRanking.find_all('div', class_='tit5')]
 score = [x.text.strip() for x in lstRanking.find_all('td', class_='point')]
-
 ranking = range(1,len(score)+1)
 link = ['https://movie.naver.com' + x.a.get('href') for x in lstRanking.find_all('div', class_='tit5')]
+
 movieTable = pd.DataFrame({'rank': ranking, 'title': title, 'score': score, 'link': link})
 movieTable['date'] = '2021-05-03'
 print(movieTable.head())
