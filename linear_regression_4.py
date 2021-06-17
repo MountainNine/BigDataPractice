a2182d604fc06665712826a5376cf3b57af041c2
mt_9 <hyunsichang@hanyang.ac.kr>
2021-06-17 14:51:36 +0900

    모델 제작

--- linear_regression_4.py
+++ linear_regression_4.py
@@ -5,10 +5,11 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
-
 train_x = pd.read_csv('data/mosquito/train_x.csv')
 train_y = pd.read_csv('data/mosquito/train_y.csv')
 test_x = pd.read_csv('data/mosquito/test_x.csv')
+print(dir(pd.DataFrame))
+
 
 def pre_processing(x):
     x['date'] = list(map(lambda i: pd.to_datetime(i), x['date']))
@@ -18,5 +19,22 @@ def pre_processing(x):
     x = pd.get_dummies(x)
 
     x = x.drop(['date', 'month'], axis=1)
+    scaler = StandardScaler()
+    x[['강수량(mm)','평균기온(℃)','최저기온(℃)','최고기온(℃)']] = scaler.fit_transform(x[['강수량(mm)','평균기온(℃)','최저기온(℃)','최고기온(℃)']])
     return x
 
+train_x = pre_processing(train_x)
+train_y = train_y.drop('date', axis=1)
+
+xgb = XGBRegressor(learning_rate=0.025, max_depth=2)
+cv_score = cross_val_score(xgb, train_x, train_y, cv=5, scoring='neg_mean_squared_error')
+cv_score_mean = np.sqrt(np.mean(cv_score) * -1)
+print(cv_score_mean)
+
+# params = {
+#     'max_depth': [2,3]
+# }
+# grid_cv = GridSearchCV(xgb, cv=5, param_grid=params, scoring='neg_mean_squared_error')
+# grid_cv.fit(train_x, train_y)
+#
+# print(grid_cv.best_params_)
