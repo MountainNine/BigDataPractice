5e7cc37d0d24aad3f4e66049346a80863ee45b98
MountainNine <goosan126@gmail.com>
2021-08-16 13:54:47 +0900

    create dam.ipynb

--- crawling.py
+++ crawling.py
@@ -16,3 +16,5 @@ link = ['https://movie.naver.com' + x.a.get('href') for x in lstRanking.find_all
 movieTable = pd.DataFrame({'rank': ranking, 'title': title, 'score': score, 'link': link})
 movieTable['date'] = '2021-05-03'
 print(movieTable.head())
+
+#https://job.alio.go.kr/recruit.do?pageNo=1&idx=&recruitYear=&recruitMonth=&detail_code=R600020&work_type=R1010&work_type=R1070&career=R2020&career=R2030&replacement=N&s_date=2018.06.01&e_date=2021.08.05&org_type=&org_name=&title=&order=REG_DATE
